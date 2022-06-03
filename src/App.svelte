4d452952ee0af65935243f42d4f704d893ea7328
remadex <s.cardon@outlook.com>
2022-06-03 10:12:37 +0200

    change width by height because it bugs on safari

--- src/App.svelte
+++ src/App.svelte
@@ -6,7 +6,7 @@
   <header
     class="mt-6 mx-8 shadow-md rounded-md flex justify-center bg-white overflow-hidden"
   >
-    <img src="/logo.png" alt="logo" class="w-28 animation-image" />
+    <img src="/logo.png" alt="logo" class="w-16 animation-image" />
   </header>
   <main class="container mx-auto m-8 border-2 border-dashed rounded p-8">
     <!-- CONTENU ICI -->
